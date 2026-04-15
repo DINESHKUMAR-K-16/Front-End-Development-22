06d9fbbe03f1c154e9dfc5d6dc92d11308a385e6
Dinesh Kumar K <160807494+DINESHKUMAR-K-16@users.noreply.github.com>
2026-04-15 19:52:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ const swiper = new Swiper(".swiper", {
   ...scrollRevealOption,
     origin: "left",
     
- });
+});
   ScrollReveal().reveal(".service__row:nth-child(2n) img", {
     ...scrollRevealOption,
     origin: "left",
